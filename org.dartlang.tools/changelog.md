92146c2aade10493ea0f1423d34ca7b472b72129
Devon Carew <devoncarew@gmail.com>
2015-05-13 16:06:34 -0700

    update the changelog

--- org.dartlang.tools/changelog.md
+++ org.dartlang.tools/changelog.md
@@ -1,5 +1,8 @@
 # Changelog
 
+### 0.0.4
+- added a preliminary Dart editor
+
 ### 0.0.3
 - auto-locate the Dart SDK
 - added the ability to run `pub get` and `pub upgrade`
--- org.dartlang.tools_build/tool/grind.dart
+++ org.dartlang.tools_build/tool/grind.dart
@@ -2,9 +2,22 @@
 import 'package:grinder/grinder.dart';
 
 // http://www.vogella.com/tutorials/EclipseTycho/article.html
-
 // mvn org.eclipse.tycho:tycho-pomgenerator-plugin:generate-poms -DgroupId=org.dartlang.tools_feature
 
-void main() {
-  print('todo:');
+main(List<String> args) => grind(args);
+
+@Task()
+updateSite() {
+	// Assume the site is built.
+
+	// Copy the site to a temp dir.
+
+	// Switch branches.
+
+	// Copy the site to the root.
+
+	// Delete the temp dir.
+
+	// Switch back to the branch.
+
 }
